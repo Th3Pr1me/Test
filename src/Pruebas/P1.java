aa5735509e137013b46109cd4eafb58c3fe5bbec
Th3Pr1me <102286226+Th3Pr1me@users.noreply.github.com>
2022-03-24 17:50:50 +0100

    Update P1.java

--- src/Pruebas/P1.java
+++ src/Pruebas/P1.java
@@ -5,6 +5,7 @@ public class P1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo");
+		System.out.prntln("Este cambio fue creado en github");
 	}
 
 }
