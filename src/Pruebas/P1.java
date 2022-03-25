becb5590c0cf0c75058e1c72e46a344aa5ebdb12
Th3Pr1me <vmartcan@MUR-FN44BK3.usersad.everis.int>
2022-03-25 11:00:03 +0100

    cambio P1

--- src/Pruebas/P1.java
+++ src/Pruebas/P1.java
@@ -4,7 +4,7 @@ public class P1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//cambio arriba
+		//Cambio arriba
 		System.out.println("Hola mundo");
 		//este estaba comentado
 		System.out.prntln("Este cambio fue creado en github");
